ff74780bc3d1503358df3a9a2fb851ba3d7dbb57
Drina <drinadomandiola@gmail.com>
2020-07-22 07:35:36 +1200

    the NewNewOne

---
+++ Summative/New Unity Project/Assets/Scripts/scenechanger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scenechanger : MonoBehaviour
+{
+    public void Load2d()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Load3d()
+    {
+        SceneManager.LoadScene(0);
+    }
+ 
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+}
